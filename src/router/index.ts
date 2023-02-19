75d936c71fbebe342883479abd063e5e481dccf1
Cervon Wong <tcrammy@gmail.com>
2023-02-19 10:23:06 +0800

    Add title and description website meta

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,36 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+import { nextTick } from "vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+    component: () => import(/* webpackChunkName: "home" */ "../views/Home.vue"),
+    meta: {
+      title: "jan Sepon's Toki Pona website",
+      description: "Welcome to my Toki Pona website!",
+    },
   },
   {
     path: "/about",
     name: "About",
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    meta: {
+      title: "About",
+      description: "Read how and why I build this Toki Pona website.",
+    },
   },
   {
     path: "/dict",
     name: "Dictionary",
     component: () =>
       import(/* webpackChunkName: "dict" */ "../views/Dictionary.vue"),
+    meta: {
+      title: "Dictionary",
+      description:
+        "Learn the meanings of each Toki Pona word, with example sentences.",
+    },
   },
 ];
 
@@ -42,4 +55,14 @@ declare module "vue-router" {
   }
 }
 
+const DEFAULT_TITLE = "jan Sepon's Toki Pona website";
+const DEFAULT_DESCRIPTION = "Welcome to my Toki Pona website!";
+router.afterEach((to) => {
+  nextTick(() => {
+    document.title = to.meta.title || DEFAULT_TITLE;
+    document
+      .querySelector('meta[name="description"]')
+      ?.setAttribute("content", to.meta.description || DEFAULT_DESCRIPTION);
+  });
+});
 export default router;
